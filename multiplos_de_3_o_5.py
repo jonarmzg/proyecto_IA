b0bdcc18f8c4231737391533c24337140afcb3d9
Jonathan Ramirez <jonathanrmzg@gmail.com>
2018-06-20 16:20:47 -0500

    Empezando

---
+++ multiplos_de_3_o_5.py
@@ -0,0 +1,12 @@
+"""Si tenemos una lista de todos los números naturales antes del 10 que son múltiplos de 3 o 5, obtenemos 3, 5, 6 y 9.
+La suma de estos múltiplos es 23. Encuentra la suma de todos los múltiplos de 3 o 5 que estén antes del 1000."""
+"""i=6
+suma=3+5
+while i<1000:
+    mod3=(i) % 3
+    mod5=(i) % 5
+    if mod3==0 or mod5==0:
+        suma=suma+(i)
+    i+=1
+
+print ("El resultado es: ",suma)"""
